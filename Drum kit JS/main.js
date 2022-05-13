0c0c41ecd0e13302690e899323fa24585d24c828
slam256 <staceylubowa@gmail.com>
2022-05-13 06:38:18 -0400

    Some chnages in the code organisation

--- Drum kit JS/main.js
+++ Drum kit JS/main.js
@@ -1,6 +1,22 @@
-window.addEventListener('keydown', function (e){
-    
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) 
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`) 
     if(!audio) return; //stop the function from running all together.
+    audio.currentTime = 0;
     audio.play();
-});
+    key.classList.add('playing');
+    //transition end event
+}
+
+function removeTransition(e) {
+   if (e.propertyName !== 'transform' ) return;
+   this.classList.remove('playing')
+}
+
+
+window.addEventListener('keydown', playSound); 
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)) //go through all the keys and when they finish transition(style sheets) then we remove the transiion.
